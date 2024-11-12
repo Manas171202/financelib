c59f934a8a44292793042aea14a92fadd56f432a
Manas171202 <114315775+Manas171202@users.noreply.github.com>
2024-11-12 20:34:53 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,36 @@
+/*
+const firebaseConfig = {
+    apiKey: "AIzaSyCmNwjTz9YFblRRyE99eaKTPrHF95AEo64",
+    authDomain: "stock-market-cbade.firebaseapp.com",
+    projectId: "stock-market-cbade",
+    storageBucket: "stock-market-cbade.appspot.com",
+    messagingSenderId: "765522212217",
+    appId: "1:765522212217:web:6693f4d34c049f941a2ba8",
+    measurementId: "G-ZFNVE7CMP9"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+  
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app); */
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbB-DZTVF9kk5ILK4UyS7rAZly2E7d7BI",
+  authDomain: "quote-app-500c8.firebaseapp.com",
+  projectId: "quote-app-500c8",
+  storageBucket: "quote-app-500c8.appspot.com",
+  messagingSenderId: "834986551116",
+  appId: "1:834986551116:web:0bd96599aa0cb4372891e9",
+  measurementId: "G-JNPGZCGK5Z"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
